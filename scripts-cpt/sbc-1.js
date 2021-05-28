0c6333e1586d4d273edcdcdffc59e44f84664a7a
tgn <trwygon@outlook.pt>
2021-05-28 21:30:58 +0100

    Door update on CPT

--- scripts-cpt/sbc-1.js
+++ scripts-cpt/sbc-1.js
@@ -4,9 +4,22 @@ var LOTACAO_SLOT = 1;
 var API_URL = "http://127.0.0.1/api/api.php";
 var lotacao;
 
+//Enviar a lotação atual para a API
 function postToServer(){
     var valores = {'nome': 'lotacao' , 'valor': lotacao};
     RealHTTPClient.post(API_URL, valores);
+}
+
+//Obter o estado da porta e abrir ou fechar se necessário
+function checkDoor(){
+	RealHTTPClient.get(API_URL + "?nome=portao", function(status, data){
+		Serial.println(status + " : " + data);
+        if(status==200)
+            if(data=="ABERTO")
+            	customWrite(DOOR_SLOT, 1);
+            else
+            	customWrite(DOOR_SLOT, 0);
+	});
 }
 
 function setup() {
@@ -18,5 +31,6 @@ function loop() {
     lotacao = customRead(LOTACAO_SLOT);
     Serial.println("L: " + lotacao);
     postToServer();
+    checkDoor();
     delay(5000);
 }
