fe29dd91a52fc75bd63fb26d3c5830d692f7b312
tgn <trwygon@outlook.pt>
2021-05-28 21:05:33 +0100

    Lotacao management

--- scripts-cpt/mcu-3.js
+++ scripts-cpt/mcu-3.js
@@ -1,9 +1,21 @@
 var TW_ENTRANCE = 1;
 var TW_EXIT = 2;
 
+var lastStateEnt = 0;
+var lastStateEx = 0;
+
 var OUTPUT_SLOT = 0;
 
-var lotacao;
+var API_URL = "http://127.0.0.1/api/api.php?nome=lotacao";
+var lotacao=0; //valor default da lotação caso ocorra algum erro ao contactar a API
+
+function getCurrentLotacao(){
+    RealHTTPClient.get(API_URL, function(status, data){
+		Serial.println(status + " : " + data);
+        if(status==200)
+            lotacao=data;
+	});
+}
 
 function setup() {
     pinMode(OUTPUT_SLOT, OUTPUT);
@@ -11,10 +23,31 @@ function setup() {
 	pinMode(TW_EXIT, INPUT);
 
     //TODO: Obter a lotação atual
-    lotacao = 10;
+    getCurrentLotacao();
 }
 
 function loop() {
-
+	var state = digitalRead(TW_ENTRANCE);
+	if(state!=lastStateEnt){
+        //Houve uma alteração no estado do sensor de entrada
+        if(state==HIGH){
+            //Alguém entrou
+            lotacao++;
+        }
+    	lastStateEnt=state;
+	}
+	
+	
+	state = digitalRead(TW_EXIT);
+	if(state!=lastStateEx){
+        //Houve uma alteração no estado do sensor de entrada
+        if(state==HIGH){
+            //Alguém saiu
+            if(lotacao!==0)
+                lotacao--;
+        }
+    	lastStateEx=state;
+	}
+	
     customWrite(OUTPUT_SLOT, lotacao.toString());
 }
--- scripts-cpt/sbc-1.js
+++ scripts-cpt/sbc-1.js
@@ -1,7 +1,13 @@
 var DOOR_SLOT = 0;
 var LOTACAO_SLOT = 1;
 
+var API_URL = "http://127.0.0.1/api/api.php";
 var lotacao;
+
+function postToServer(){
+    var valores = {'nome': 'lotacao' , 'valor': lotacao};
+    RealHTTPClient.post(API_URL, valores);
+}
 
 function setup() {
 	pinMode(DOOR_SLOT, OUTPUT);
@@ -10,4 +16,7 @@ function setup() {
 
 function loop() {
     lotacao = customRead(LOTACAO_SLOT);
+    Serial.println("L: " + lotacao);
+    postToServer();
+    delay(5000);
 }
