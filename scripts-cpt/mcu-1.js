f1089bb8a838aec2091c6c7093447adc0084a30c
tgn <trwygon@outlook.pt>
2021-05-30 18:22:40 +0100

    Structure (API Communication)

--- scripts-cpt/mcu-1.js
+++ scripts-cpt/mcu-1.js
@@ -5,6 +5,27 @@ var CO2_SLOT = 1;
 var LIGHT_SLOTS = [2, 3];
 var AC_SLOT = 4;
 
+var API_URL = "http://127.0.0.1/api/api.php";
+
+//Enviar os dados de um sensor/atuador à API
+function postToAPI(nome, valor){
+    var valores = {'nome': nome , 'valor': valor};
+    RealHTTPClient.post(API_URL, valores);
+}
+
+//variavel temporaria utilizada para obter o output da funcao getFromAPI
+var temp_var = 0;
+
+//Obter os dados de um atuador da API
+function getFromAPI(nome){
+    RealHTTPClient.get(API_URL+"?nome="+nome, function(status, data){
+		Serial.println(status + " : " + data);
+        if(status==200)
+            temp_var=data;
+	});
+}
+
+//Obter a temperatura a partir do sensor
 function lerTemperatura() {
 	var temp = analogRead(TEMP_SLOT);
 	temp = temp*200/1023.0;
