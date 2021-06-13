76f92737efe0d1021f001f17328b76a7279833c5
tgn <trwygon@outlook.pt>
2021-06-13 17:43:27 +0100

    Webcam verifications

--- website/api/cam.php
+++ website/api/cam.php
@@ -4,27 +4,43 @@ $THE_PATH = "files/webcam/webcam.jpg";
 
 //Guardar a imagem
 function uploadImg($img, $path){
+
+    //Verificar se o ficheiro não excede o limite de tamanho
+    if($img['size']>1000000){
+        http_response_code(400);
+        exit("A imagem excede o tamanho máximo de 1000kB.");
+    }
+
     //Verificar se oo ficheiro é uma imagem
     $check = getimagesize($img["tmp_name"]);
     if($check == false) {
         http_response_code(400);
-        echo "File is not an image.";
+        exit("O ficheiro não é uma imagem.");
+    }
+    
+    //Verificar se é do tipo permitido de imagens
+    $allowed = array('png', 'jpg');
+    $filename = $img["name"];
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    if (!in_array($ext, $allowed)) {
+        http_response_code(400);
+        exit("O ficheiro não é uma imagem JPG ou PNG.");
     }
 
     //Mover o ficheiro para uma localização permanente
     if (move_uploaded_file($img["tmp_name"], $path)) {
         http_response_code(200);
-        echo "Image has been uploaded.";
+        echo "A Imagem foi uploaded.";
     } else {
         http_response_code(405);
-        echo "There was an error uploading the image.";
+        exit("Ocorreu um erro ao tentar efetuar o upload.");
     }
 }
 
 if($_SERVER['REQUEST_METHOD']!='POST'){
     if($_SERVER['REQUEST_METHOD']!='GET'){
         http_response_code(403);
-        echo "metodo nao permitido";
+        exit("Método nao permitido");
     }
 
     //é um GET, ou seja é para obter o modo da camera
@@ -48,7 +64,7 @@ if(isset($_POST['modo'])){
 else if(!isset($_FILES['imagem'])){
     //Se tambem não tiver sido enviado um ficheiro, o request é invalido
     http_response_code(400);
-    echo "imagem nao definida";
+    echo "Imagem não definida";
     return;
 }
 
